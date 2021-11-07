39a04cf95af6a40cfa00c02228f7f67cf47b136d
zezinho <osmanissa94@gmail.com>
2021-11-07 19:03:46 -0300

    Implementado o front-end dp loading e tratamento de erros

--- src/pages/peoplePage.js
+++ src/pages/peoplePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text, View, ActivityIndicator, StyleSheet } from 'react-native';
 import axios from "axios";
 import PeopleList from "../components/PeopleList";
 
@@ -9,28 +9,61 @@ export default class PeoplePage extends React.Component
         super(props);
 
         this.state = {
-            people: []
+            people: [],
+            loading: false,
+            error: false
         }
     }
 
     componentDidMount() {
-        axios.get('https://randomuser.me/api/?nat=br&results=5')
-            .then(response => {
-                const { results } = response.data;
-                this.setState({
-                    people: results
+        this.setState({ loading: true })
+        setTimeout(() => {
+            axios
+                .get('https://randomuser.me/api/?nat=br&results=15')
+                .then(response => {
+                    const {results} = response.data;
+                    this.setState({
+                        people: results,
+                        loading: false
+                    })
+                }).catch(error => {
+                    this.setState({ loading: false, error: true })
                 })
-            })
+        }, 3500)
+    }
+
+    renderPage() {
+        if(this.state.loading)
+            return <ActivityIndicator size="large" color="#000" />
+
+        if(this.state.error)
+            return <Text style={ styles.error }>Oops... Algo deu errado =(</Text>
+
+        return (
+            <PeopleList people={this.state.people}
+            onPressItem={pageParams => (
+                this.props.navigation.navigate('PersonDetails', pageParams)
+            )} />
+        )
     }
 
     render() {
         return (
-            <View>
-                <PeopleList people={this.state.people}
-                onPressItem={pageParams => (
-                    this.props.navigation.navigate('PersonDetails', pageParams)
-                )} />
+            <View style={ styles.container }>
+                { this.renderPage() }
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center"
+    },
+    error: {
+        fontSize: 20,
+        color: "red",
+        alignSelf: "center"
+    }
+})
